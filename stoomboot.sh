b0b7ed2895737e23c747b908c9efdb32de55d3ae
Nathan Hartland <nhartlan@stbc-i2.nikhef.nl>
2017-09-08 14:12:29 +0200

    small fixes to stoomboot script

--- stoomboot.sh
+++ stoomboot.sh
@@ -1,8 +1,8 @@
 echo "Launching " $1 " jobs of configuration " $2
 
-for (( c=1; c<=$1; c++ ))
+for (( c=1; c<$1; c++ ))
 do  
    echo "cd ${PWD}
-./dPDF ${c} ${2}" > ${c}.run
+./dPDF ${2} ${c}" > ${c}.run
    qsub -q short ${c}.run
 done
